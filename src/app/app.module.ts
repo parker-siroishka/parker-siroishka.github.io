d128452a78be83475d65a09debd66e35b7e47e2a
Parker Siroishka <parkersir10@hotmail.com>
2019-12-21 17:33:56 -0700

    Initial Routing Complete
    App now routes to ProfileComponent on init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+import { HttpClientModule } from '@angular/common/http';
+import { Profile } from 'selenium-webdriver/firefox';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, config),
+    ProfileModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
