863086bfa179a0661d5f1c3d77bf3cce02bc29f4
Parker Siroishka <parkersir10@hotmail.com>
2019-12-29 23:14:58 -0700

    Initialized header and added some introduction photos
    All photos are unformated

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, IntroComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    IntroComponent
   ]
 })
 export class ProfileModule { }
